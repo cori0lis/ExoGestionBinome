d3c6df6a6c8171632b9047497eb3a44a5c757948
cori0lis <67338651+cori0lis@users.noreply.github.com>
2020-11-23 15:18:34 +0100

    list stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Stagiaire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
+    // /**
+    //  * @Route("/stagiaire", name="stagiaire")
+    //  */
+    // public function index(): Response
+    // {
+    //     return $this->render('stagiaire/index.html.twig', [
+    //         'controller_name' => 'StagiaireController',
+    //     ]);
+    // }
     /**
      * @Route("/stagiaire", name="stagiaire")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
-        ]);
+        $stagiaires = $this->getDoctrine()
+            ->getRepository(Stagiaire::class)
+            ->getAll();
+
+        return $this->render('stagiaire/index.html.twig', ['stagiaires' => $stagiaires,]);
     }
 }
