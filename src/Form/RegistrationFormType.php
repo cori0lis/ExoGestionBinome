a8e38d1f9a63c8fa673526f9bce31e55af609f39
Iann <beniann@hotmail.fr>
2020-11-24 14:52:03 +0100

    Registration update

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,32 +11,45 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+            ->add('email', EmailType::class, [
+                'required' => true
             ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            // ->add('plainPassword', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
             ->add('roles', ChoiceType::class, [
                 'attr' => [
                     'class' => 'selectpicker',
@@ -47,19 +60,36 @@ class RegistrationFormType extends AbstractType
                     'admin' => 'ROLE_ADMIN'
                 ],
                 'multiple' => true,
+                'required' => true,
             ])
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class, [
+                'required' => true
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true
+            ])
             ->add('datenaissance', DateType::class, [
                 'years' => range(date('Y'), date('Y') - 70),
                 'label' => 'Date de naissance',
-                'format' => 'ddMMMMyyyy'
+                'format' => 'ddMMMMyyyy',
+                'required' => true
             ])
-            ->add('civilite', TextType::class)
-            ->add('telephone', TextType::class)
-            ->add('adresse', TextType::class)
-            ->add('cp', TextType::class)
-            ->add('ville', TextType::class)
+            ->add('civilite', TextType::class, [
+                'required' => true
+            ])
+            ->add('telephone', TextType::class, [
+                'required' => true
+            ])
+            ->add('adresse', TextType::class, [
+                'required' => true
+            ])
+            ->add('cp', TextType::class, [
+                'required' => true
+            ])
+            ->add('ville', TextType::class, [
+                'required' => true
+            ])
+            ->add('valider', SubmitType::class)
             // ->add('agreeTerms', CheckboxType::class, [
             //     'mapped' => false,
             //     'constraints' => [
