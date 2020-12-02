2cbe3831bfbaa811f07daaf2001a344fb906bff4
cori0lis <67338651+cori0lis@users.noreply.github.com>
2020-12-02 11:43:49 +0100

    suite collectiontype

--- src/Form/ModulesType.php
+++ src/Form/ModulesType.php
@@ -2,21 +2,31 @@
 
 namespace App\Form;
 
+use App\Form\DureeType;
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class ModulesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('durees', CollectionType::class [
-                // 'entry_type' => DureeType::class
+            ->add('durees', CollectionType::class, [
+                'label' => false,
+                'entry_type' => DureeType::class,
+                'entry_options' => [
+                    'label' => "Atelier et Duree"
+                ],
+                'allow_add' => true,
+                'allors_delete' => true,
+                'by_reference' => false
             ])
             // ->add('description')
+            ->add('valider', SubmitType::class);
         ;
     }
 
