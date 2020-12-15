859cc65d897864069bd5d6225b575b0114d00282
cori0lis <67338651+cori0lis@users.noreply.github.com>
2020-12-15 09:42:26 +0100

    calendrier propre

---
+++ public/js/calendar.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var calendarEl = document.getElementById('calendar-holder');
+
+var calendar = new FullCalendar.Calendar(calendarEl, {
+// eventClick: function(info) {
+// window.open("/formateur/session/"+el.id);
+// },
+navLinks: true,
+weekNumbers: true,
+defaultView: 'dayGridMonth',
+editable: true,
+eventSources: [
+    {
+        url: "/fc-load-events",
+        method: "POST",
+        extraParams: {
+            filters: JSON.stringify({})
+        },
+        failure: () => {
+            // alert("There was an error while fetching FullCalendar!");
+        },
+    },
+],
+header: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+},
+plugins: [ 'interaction', 'dayGrid', 'timeGrid' ], // https://fullcalendar.io/docs/plugin-index
+timeZone: 'UTC',
+});
+calendar.render();
+});
